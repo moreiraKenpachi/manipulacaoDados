3b0594993cdcde2415b1338811b79e1a3464b78f
fabricio <fabriciobonete@yahoo.com.br>
2023-09-05 16:59:06 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,71 @@
+/* meu comentário*/
+
+console.log("bem vindos") // meu comentario para código
+console.log('muito obrigado')
+console.log(undefined)
+console.log(null)
+
+/* 
+estudando object
+{propriedade:"valor"}
+*/
+console.log({
+    name:"Marcos",
+    idade:32,
+    andar: function(){
+        console.log('andar')
+    }
+})
+
+/* Array*/
+
+console.log([
+    "Marcos",
+    "bantene",
+    "cruz"
+])
+
+/* fazendo Exercício para aprender*/
+
+//let weight
+//console.log(typeof weight)
+
+let student = {
+    name: "Marcos",
+    age: 27,
+    stars: 20,
+    isSubscribed: true
+}
+//console.log(student)*/
+
+// criando um texto
+
+/*let student = {
+    name: "Marcos"",
+    age: 27,
+    stars: 20,
+    isSubscribed: true
+}
+console.log(`meu nome é ${student.name}, cuja idade é ${student.age}, um estrela muito pouco ${student.stars}.`)*/
+
+//criando um texto
+
+// let student[]
+/*let students = [
+    student
+]
+console.log(students)
+console.log(students[0])*/
+
+/*const john = {
+    name: "john",
+    age: 25,
+    weight: 23,
+    isSubscribed: true,
+}
+
+students[0] = john
+console.log(students)*/
+
+console.log(a)
+let a = 1
